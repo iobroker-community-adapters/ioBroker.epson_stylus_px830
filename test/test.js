5f36842c554248b61bd0befe6024d2a435878dc3
Ingo Fischer <github@fischer-ka.de>
2022-01-29 15:27:36 +0100

    update testAdapter.js

--- test/test.js
+++ test/test.js
@@ -9,13 +9,13 @@ describe('epson_stylus_px830: test adapter', function() {
     before('epson_stylus_px830: Start js-controller', function (_done) {
         this.timeout(600000); // because of first install from npm
 
-        setup.setupController(function () {
+        setup.setupController(async function () {
             var config = setup.getAdapterConfig();
             // enable adapter
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
 
-            setup.setAdapterConfig(config.common, config.native);
+            await setup.setAdapterConfig(config.common, config.native);
 
             setup.startController(function (_objects, _states) {
                 objects = _objects;
